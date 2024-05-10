87ddfbaca1f7ff616bd0cacbaeb130de4d72f6f9
Natalia Lozano <acuarionat2004@gmail.com>
2024-05-10 09:20:00 -0400

    composicion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css'
+import Tarjeta from './componentes/Tarjeta'
 
 function App() {
   return (
     <>
+    <Tarjeta/>
     </>
   )
 }
---
+++ src/componentes/Tarjeta.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Texto from './Texto';
+import Video from './Video';
+
+const Tarjeta = () => {
+  return (
+    <div className='tarjeta'>
+        <Texto/>
+        <Video/>
+    </div>
+  )
+}
+
+export default Tarjeta
