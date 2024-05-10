e388e24ca3a9db77fa694741042e2df391ed13d4
Natalia Lozano <acuarionat2004@gmail.com>
2024-05-10 09:11:42 -0400

    primer componente

---
+++ src/componentes/Texto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Texto = () => {
+  return (
+    <div className='texto'>
+        <h2>Terapia</h2>
+        <p>Ejercicios pelvicos</p>
+    </div>
+  )
+}
+
+export default Texto
