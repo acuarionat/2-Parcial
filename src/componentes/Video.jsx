7d906c570c9fc2b211fcdf592fecdcbe01caf571
Natalia Lozano <acuarionat2004@gmail.com>
2024-05-10 09:15:45 -0400

    segundo componente

---
+++ src/componentes/Video.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <div className='video'>
+        <img src={"/public/Rectangle.svg"} alt={"ejercicio"} />
+        <div className="play"> <img src={"/public/triangle.svg" }alt={"play"} /> </div>
+    </div>
+  )
+}
+
+export default Video
